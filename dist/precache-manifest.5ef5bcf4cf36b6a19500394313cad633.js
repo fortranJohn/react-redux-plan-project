c18113b2734dc49177c8387317eb4c38da77e158
Temitope Ogunbiyi <ogunbiyitemitopeayodele@gmail.comgit config --global user.email ogunbiyitemitopeayodele@gmail.comgit config --global user.email ogunbiyitemitopeayodele@gmail.com>
2020-11-17 15:55:09 +0100

    Second commit

---
+++ dist/precache-manifest.5ef5bcf4cf36b6a19500394313cad633.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b8cf0094f15782b7aa29d7f5dca64f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "055aebc06741d8e692ec",
+    "url": "/static/css/main.a57808a7.chunk.css"
+  },
+  {
+    "revision": "a6f2e026f14314d2246f",
+    "url": "/static/js/2.c502c204.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/static/js/2.c502c204.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "055aebc06741d8e692ec",
+    "url": "/static/js/main.b927228a.chunk.js"
+  },
+  {
+    "revision": "cf3bf6e5893ab5d45add",
+    "url": "/static/js/runtime-main.13abf10c.js"
+  }
+]);
